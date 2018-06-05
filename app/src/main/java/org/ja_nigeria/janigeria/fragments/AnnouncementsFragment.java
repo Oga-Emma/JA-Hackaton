af65c7083fa03d23876e779cf8f5400e265f2dd9
Ugochukwu Ofoegbu <ofoegbuv@gmail.com>
2018-06-05 05:34:46 +0100

    notification item added RecyclerView Added to Notifications

--- app/src/main/java/org/ja_nigeria/janigeria/fragments/AnnouncementsFragment.java
+++ app/src/main/java/org/ja_nigeria/janigeria/fragments/AnnouncementsFragment.java
@@ -3,9 +3,12 @@ package org.ja_nigeria.janigeria.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.ja_nigeria.janigeria.R;
 
@@ -13,6 +16,9 @@ import org.ja_nigeria.janigeria.R;
  * A simple {@link Fragment} subclass.
  */
 public class AnnouncementsFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private ProgressBar mProgress;
+    private TextView mTextView;
 
 
     public AnnouncementsFragment() {
@@ -28,7 +34,12 @@ public class AnnouncementsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_announcements, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_announcements, container, false);
+        mRecyclerView = rootView.findViewById(R.id.announcement_recycler_view);
+
+
+
+        return rootView;
     }
 
 }
